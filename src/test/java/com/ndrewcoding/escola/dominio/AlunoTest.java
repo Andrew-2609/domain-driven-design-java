14398cb44dd5e5b2d87c3b9e0eeb3b65024fe3ec
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-15 08:33:49 -0300

    Test: implement new test 'deveriaPermitirAdicionarUmTelefone' along with a BeforeEach method in AlunoTest

--- src/test/java/com/ndrewcoding/escola/dominio/AlunoTest.java
+++ src/test/java/com/ndrewcoding/escola/dominio/AlunoTest.java
@@ -1,4 +1,31 @@
 package com.ndrewcoding.escola.dominio;
 
+import com.ndrewcoding.escola.dominio.aluno.Aluno;
+import com.ndrewcoding.escola.dominio.aluno.CPF;
+import com.ndrewcoding.escola.dominio.aluno.Email;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AlunoTest {
+
+    private Aluno aluno;
+
+    @BeforeEach
+    void setUp() {
+        this.aluno = new Aluno(
+                new CPF("123.456.789-00"),
+                "Andrew",
+                new Email("andrew@email.com")
+        );
+    }
+
+    @Test
+    void deveriaPermitirAdicionarUmTelefone() {
+        this.aluno.adicionarTelefone("85", "91234-5678");
+        
+        assertEquals(1, this.aluno.getTelefones().size());
+    }
+
 }
