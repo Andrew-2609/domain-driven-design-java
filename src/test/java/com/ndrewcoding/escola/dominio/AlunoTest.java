b5bdbf3d712a7e00fb6c69b4b11c1628f0bf5060
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-15 08:40:08 -0300

    Test: implement new test 'naoDeveriaPermitirAdicionarTresTelefones' in AlunoTest

--- src/test/java/com/ndrewcoding/escola/dominio/AlunoTest.java
+++ src/test/java/com/ndrewcoding/escola/dominio/AlunoTest.java
@@ -3,10 +3,12 @@ package com.ndrewcoding.escola.dominio;
 import com.ndrewcoding.escola.dominio.aluno.Aluno;
 import com.ndrewcoding.escola.dominio.aluno.CPF;
 import com.ndrewcoding.escola.dominio.aluno.Email;
+import com.ndrewcoding.escola.dominio.aluno.LimiteDeTelefonesAtingidoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AlunoTest {
 
@@ -34,6 +36,14 @@ public class AlunoTest {
         this.aluno.adicionarTelefone("85", "95678-9012");
 
         assertEquals(2, this.aluno.getTelefones().size());
+    }
+
+    @Test
+    void naoDeveriaPermitirAdicionarTresTelefones() {
+        this.aluno.adicionarTelefone("85", "91234-5678");
+        this.aluno.adicionarTelefone("85", "95678-9012");
+
+        assertThrows(LimiteDeTelefonesAtingidoException.class, () -> this.aluno.adicionarTelefone("85", "93456-7890"));
     }
 
 }
