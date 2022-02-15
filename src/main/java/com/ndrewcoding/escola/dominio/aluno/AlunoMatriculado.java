917ed0761744cda8ee6fe87c88918c5fa3cb8c02
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-15 09:06:27 -0300

    Feat: implement basic structure of AlunoMatriculado

--- src/main/java/com/ndrewcoding/escola/dominio/aluno/AlunoMatriculado.java
+++ src/main/java/com/ndrewcoding/escola/dominio/aluno/AlunoMatriculado.java
@@ -6,9 +6,21 @@ import java.time.LocalDateTime;
 
 public class AlunoMatriculado implements Evento {
 
+    private final CPF cpfDoAluno;
+    private final LocalDateTime momento;
+
+    public AlunoMatriculado(CPF cpfDoAluno) {
+        this.cpfDoAluno = cpfDoAluno;
+        this.momento = LocalDateTime.now();
+    }
+
     @Override
     public LocalDateTime momento() {
-        return null;
+        return this.momento;
+    }
+
+    public CPF getCpfDoAluno() {
+        return cpfDoAluno;
     }
 
 }
