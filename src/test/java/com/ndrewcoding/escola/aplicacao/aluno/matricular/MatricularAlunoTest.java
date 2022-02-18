647da220ed430e3ecc11d2ced2e5579206a5f56e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 22:33:12 -0300

    Test: add a PublicadorDeEventos() object in MatricularAlunoTest

--- src/test/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAlunoTest.java
+++ src/test/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAlunoTest.java
@@ -1,7 +1,10 @@
 package com.ndrewcoding.escola.aplicacao.aluno.matricular;
 
+import com.ndrewcoding.escola.dominio.PublicadorDeEventos;
 import com.ndrewcoding.escola.dominio.aluno.CPF;
+import com.ndrewcoding.escola.dominio.aluno.LogDeAlunoMatriculado;
 import com.ndrewcoding.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,9 +12,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MatricularAlunoTest {
 
+    private static final PublicadorDeEventos PUBLICADOR_DE_EVENTOS = new PublicadorDeEventos();
+
+    @BeforeAll
+    static void setUp() {
+        PUBLICADOR_DE_EVENTOS.adicionar(new LogDeAlunoMatriculado());
+    }
+
     @Test
     void alunoDeveriaSerPersistido() {
-        MatricularAluno useCase = new MatricularAluno(new RepositorioDeAlunosEmMemoria());
+        MatricularAluno useCase = new MatricularAluno(new RepositorioDeAlunosEmMemoria(), PUBLICADOR_DE_EVENTOS);
 
         MatricularAlunoDTO dto = new MatricularAlunoDTO("123.456.789-00", "Andrew Monteiro", "andrew@email.com");
 
