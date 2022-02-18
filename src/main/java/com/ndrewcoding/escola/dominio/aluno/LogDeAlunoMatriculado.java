b2ddbd053c128ba9f5fe538a268053aabd48c533
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 22:14:37 -0300

    Feat: make LogDeAlunoMatriculado a subclass of Ouvinte

--- src/main/java/com/ndrewcoding/escola/dominio/aluno/LogDeAlunoMatriculado.java
+++ src/main/java/com/ndrewcoding/escola/dominio/aluno/LogDeAlunoMatriculado.java
@@ -1,13 +1,22 @@
 package com.ndrewcoding.escola.dominio.aluno;
 
+import com.ndrewcoding.escola.dominio.Evento;
+import com.ndrewcoding.escola.dominio.Ouvinte;
+
 import java.time.format.DateTimeFormatter;
 
-public class LogDeAlunoMatriculado {
+public class LogDeAlunoMatriculado extends Ouvinte {
 
-    public void reageAo(AlunoMatriculado evento) {
+    @Override
+    public void reageAo(Evento evento) {
         String momentoFormatado = evento.momento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
-        String log = String.format("Aluno com CPF %s, matriculado em %s", evento.getCpfDoAluno(), momentoFormatado);
+        String log = String.format("Aluno com CPF %s, matriculado em %s", ((AlunoMatriculado) evento).getCpfDoAluno(), momentoFormatado);
         System.out.println(log);
+    }
+
+    @Override
+    protected boolean deveProcessar(Evento evento) {
+        return evento instanceof AlunoMatriculado;
     }
 
 }
