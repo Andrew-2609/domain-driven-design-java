c4cf170d1a018e833d08d71af55c8f5bea52ea6e
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 22:08:02 -0300

    Feat: implement method 'reageAo()' in LogDeAlunoMatriculado

--- src/main/java/com/ndrewcoding/escola/dominio/aluno/LogDeAlunoMatriculado.java
+++ src/main/java/com/ndrewcoding/escola/dominio/aluno/LogDeAlunoMatriculado.java
@@ -1,4 +1,13 @@
 package com.ndrewcoding.escola.dominio.aluno;
 
+import java.time.format.DateTimeFormatter;
+
 public class LogDeAlunoMatriculado {
+
+    public void reageAo(AlunoMatriculado evento) {
+        String momentoFormatado = evento.momento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        String log = String.format("Aluno com CPF %s, matriculado em %s", evento.getCpfDoAluno(), momentoFormatado);
+        System.out.println(log);
+    }
+
 }
