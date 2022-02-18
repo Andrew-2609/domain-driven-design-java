c666b1f67b4ba4edf16380d1623fe2d7ebd658c3
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 22:24:14 -0300

    Feat: implement event and publisher features in MatricularAluno executar() method

--- src/main/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAluno.java
+++ src/main/java/com/ndrewcoding/escola/aplicacao/aluno/matricular/MatricularAluno.java
@@ -1,6 +1,8 @@
 package com.ndrewcoding.escola.aplicacao.aluno.matricular;
 
+import com.ndrewcoding.escola.dominio.PublicadorDeEventos;
 import com.ndrewcoding.escola.dominio.aluno.Aluno;
+import com.ndrewcoding.escola.dominio.aluno.AlunoMatriculado;
 import com.ndrewcoding.escola.dominio.aluno.CPF;
 import com.ndrewcoding.escola.dominio.aluno.RepositorioDeAlunos;
 
@@ -10,14 +12,19 @@ import java.util.stream.Collectors;
 public class MatricularAluno {
 
     private final RepositorioDeAlunos repositorioDeAlunos;
+    private final PublicadorDeEventos publicadorDeEventos;
 
-    public MatricularAluno(RepositorioDeAlunos repositorioDeAlunos) {
+    public MatricularAluno(RepositorioDeAlunos repositorioDeAlunos, PublicadorDeEventos publicadorDeEventos) {
         this.repositorioDeAlunos = repositorioDeAlunos;
+        this.publicadorDeEventos = publicadorDeEventos;
     }
 
     public void executar(MatricularAlunoDTO matricularAlunoDTO) {
         Aluno novoAluno = matricularAlunoDTO.criarAluno();
         repositorioDeAlunos.matricular(novoAluno);
+
+        AlunoMatriculado eventoAlunoMatriculado = new AlunoMatriculado(novoAluno.getCpf());
+        publicadorDeEventos.publicar(eventoAlunoMatriculado);
     }
 
     public MatricularAlunoDTO buscarPorCPF(CPF cpf) {
