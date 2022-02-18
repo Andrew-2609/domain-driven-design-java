9b10e45ad4e36a23d5d3e1c1a491ca3251332fec
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 22:11:54 -0300

    Feat: implement basic methods and method signatures of Ouvinte

--- src/main/java/com/ndrewcoding/escola/dominio/Ouvinte.java
+++ src/main/java/com/ndrewcoding/escola/dominio/Ouvinte.java
@@ -1,4 +1,15 @@
 package com.ndrewcoding.escola.dominio;
 
 public abstract class Ouvinte {
+
+    public void processa(Evento evento) {
+        if (this.deveProcessar(evento)) {
+            this.reageAo(evento);
+        }
+    }
+
+    protected abstract boolean deveProcessar(Evento evento);
+
+    protected abstract void reageAo(Evento evento);
+
 }
