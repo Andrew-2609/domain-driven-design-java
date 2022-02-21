92654db6f766d42c3ade490c485ce7379ce2b782
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 20:25:35 -0300

    Feat: override each method of RepositorioDeSelos in RepositorioDeSelosEmMemoria

--- src/main/java/com/ndrewcoding/escola/infra/selo/RepositorioDeSelosEmMemoria.java
+++ src/main/java/com/ndrewcoding/escola/infra/selo/RepositorioDeSelosEmMemoria.java
@@ -1,26 +1,32 @@
 package com.ndrewcoding.escola.infra.selo;
 
-import com.ndrewcoding.escola.dominio.aluno.CPF;
 import com.ndrewcoding.escola.dominio.selo.RepositorioDeSelos;
 import com.ndrewcoding.escola.dominio.selo.Selo;
+import com.ndrewcoding.escola.dominio.selo.SeloNaoEncontradoPorNomeException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioDeSelosEmMemoria implements RepositorioDeSelos {
 
-    @Override
-    public void cadastrar(CPF cpfDoAluno) {
+    private final List<Selo> selos = new ArrayList<>();
 
+    @Override
+    public void cadastrar(Selo selo) {
+        this.selos.add(selo);
     }
 
     @Override
     public Selo buscarPorNome(String nome) {
-        return null;
+        return selos.stream()
+                .filter(selo -> selo.getNome().equals(nome))
+                .findFirst()
+                .orElseThrow(() -> new SeloNaoEncontradoPorNomeException(nome));
     }
 
     @Override
     public List<Selo> listarTodosOsSelosCadastrados() {
-        return null;
+        return this.selos;
     }
-    
+
 }
