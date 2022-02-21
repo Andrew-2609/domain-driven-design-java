2237ba80a89386440f96d6c35d7d2033716ca230
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 20:11:11 -0300

    Feat: implement basic structure of Selo

--- src/main/java/com/ndrewcoding/escola/dominio/selo/Selo.java
+++ src/main/java/com/ndrewcoding/escola/dominio/selo/Selo.java
@@ -1,4 +1,23 @@
 package com.ndrewcoding.escola.dominio.selo;
 
+import com.ndrewcoding.escola.dominio.aluno.CPF;
+
 public class Selo {
+
+    private final CPF cpfDoAluno;
+    private final String nome;
+
+    public Selo(CPF cpfDoAluno, String nome) {
+        this.cpfDoAluno = cpfDoAluno;
+        this.nome = nome;
+    }
+
+    public CPF getCpfDoAluno() {
+        return cpfDoAluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
 }
