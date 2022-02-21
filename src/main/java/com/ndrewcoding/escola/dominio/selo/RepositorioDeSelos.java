fe84140c6dbe733e5ab07952235d1c4f271dcd22
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 20:20:16 -0300

    Chore: change cadastrar() method signature of RepositorioDeSelos

--- src/main/java/com/ndrewcoding/escola/dominio/selo/RepositorioDeSelos.java
+++ src/main/java/com/ndrewcoding/escola/dominio/selo/RepositorioDeSelos.java
@@ -1,13 +1,11 @@
 package com.ndrewcoding.escola.dominio.selo;
 
-import com.ndrewcoding.escola.dominio.aluno.CPF;
-
 import java.util.List;
 
 public interface RepositorioDeSelos {
 
-    void cadastrar(CPF cpfDoAluno);
-    
+    void cadastrar(Selo selo);
+
     Selo buscarPorNome(String nome);
 
     List<Selo> listarTodosOsSelosCadastrados();
