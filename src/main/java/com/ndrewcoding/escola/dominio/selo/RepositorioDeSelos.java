72f3ec155be7b44ac22dbd4d072fc60d0fa443ad
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 20:13:05 -0300

    Feat: create method signatures of RepositorioDeSelos

--- src/main/java/com/ndrewcoding/escola/dominio/selo/RepositorioDeSelos.java
+++ src/main/java/com/ndrewcoding/escola/dominio/selo/RepositorioDeSelos.java
@@ -1,4 +1,15 @@
 package com.ndrewcoding.escola.dominio.selo;
 
+import com.ndrewcoding.escola.dominio.aluno.CPF;
+
+import java.util.List;
+
 public interface RepositorioDeSelos {
+
+    void cadastrar(CPF cpfDoAluno);
+    
+    Selo buscarPorNome(String nome);
+
+    List<Selo> listarTodosOsSelosCadastrados();
+
 }
