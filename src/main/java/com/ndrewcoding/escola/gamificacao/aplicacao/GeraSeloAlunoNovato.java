b54bdf44ffb494c858e93232c42df94acb03faa0
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 20:52:14 -0300

    Feat: make GeraSeloAlunoNovato a subclass of Ouvinte

--- src/main/java/com/ndrewcoding/escola/gamificacao/aplicacao/GeraSeloAlunoNovato.java
+++ src/main/java/com/ndrewcoding/escola/gamificacao/aplicacao/GeraSeloAlunoNovato.java
@@ -1,4 +1,18 @@
 package com.ndrewcoding.escola.gamificacao.aplicacao;
 
-public class GeraSeloAlunoNovato {
+import com.ndrewcoding.escola.academico.dominio.Evento;
+import com.ndrewcoding.escola.academico.dominio.Ouvinte;
+
+public class GeraSeloAlunoNovato extends Ouvinte {
+
+    @Override
+    protected boolean deveProcessar(Evento evento) {
+        return false;
+    }
+
+    @Override
+    protected void reageAo(Evento evento) {
+
+    }
+
 }
