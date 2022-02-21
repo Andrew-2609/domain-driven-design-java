81cdcfdaa604e415645daff0b6c541fba772a21a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 20:54:44 -0300

    Feat: override the method deveProcessar() of GeraSeloAlunoNovato

--- src/main/java/com/ndrewcoding/escola/gamificacao/aplicacao/GeraSeloAlunoNovato.java
+++ src/main/java/com/ndrewcoding/escola/gamificacao/aplicacao/GeraSeloAlunoNovato.java
@@ -1,18 +1,19 @@
 package com.ndrewcoding.escola.gamificacao.aplicacao;
 
+import com.ndrewcoding.escola.academico.dominio.aluno.AlunoMatriculado;
 import com.ndrewcoding.escola.shared.dominio.evento.Evento;
 import com.ndrewcoding.escola.shared.dominio.evento.Ouvinte;
 
 public class GeraSeloAlunoNovato extends Ouvinte {
 
     @Override
-    protected boolean deveProcessar(Evento evento) {
-        return false;
+    protected void reageAo(Evento evento) {
+
     }
 
     @Override
-    protected void reageAo(Evento evento) {
-
+    protected boolean deveProcessar(Evento evento) {
+        return evento instanceof AlunoMatriculado;
     }
 
 }
