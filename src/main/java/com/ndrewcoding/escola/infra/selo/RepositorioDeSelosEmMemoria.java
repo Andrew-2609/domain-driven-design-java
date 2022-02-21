70665bfe175278d1589c257ef98dbf11cde69885
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 20:18:16 -0300

    Feat: implement RepositorioDeSelos interface in RepositorioDeSelosEmMemoria

--- src/main/java/com/ndrewcoding/escola/infra/selo/RepositorioDeSelosEmMemoria.java
+++ src/main/java/com/ndrewcoding/escola/infra/selo/RepositorioDeSelosEmMemoria.java
@@ -1,4 +1,26 @@
 package com.ndrewcoding.escola.infra.selo;
 
-public class RepositorioDeSelosEmMemoria {
+import com.ndrewcoding.escola.dominio.aluno.CPF;
+import com.ndrewcoding.escola.dominio.selo.RepositorioDeSelos;
+import com.ndrewcoding.escola.dominio.selo.Selo;
+
+import java.util.List;
+
+public class RepositorioDeSelosEmMemoria implements RepositorioDeSelos {
+
+    @Override
+    public void cadastrar(CPF cpfDoAluno) {
+
+    }
+
+    @Override
+    public Selo buscarPorNome(String nome) {
+        return null;
+    }
+
+    @Override
+    public List<Selo> listarTodosOsSelosCadastrados() {
+        return null;
+    }
+    
 }
