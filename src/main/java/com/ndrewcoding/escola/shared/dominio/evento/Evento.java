de41a7d941008c02881114d1793f1f8a140e0fe8
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 21:01:07 -0300

    Feat: create 'informacoes()' method signature in Evento

--- src/main/java/com/ndrewcoding/escola/shared/dominio/evento/Evento.java
+++ src/main/java/com/ndrewcoding/escola/shared/dominio/evento/Evento.java
@@ -1,11 +1,14 @@
 package com.ndrewcoding.escola.shared.dominio.evento;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public interface Evento {
 
     LocalDateTime momento();
 
     TipoDeEvento tipo();
+
+    Map<String, Object> informacoes();
 
 }
