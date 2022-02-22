141034d5551b4bd580197e45a3212c2fe2bb1389
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 21:05:26 -0300

    Feat: implement remaining methods of GeraSeloAlunoNovato

--- src/main/java/com/ndrewcoding/escola/gamificacao/aplicacao/GeraSeloAlunoNovato.java
+++ src/main/java/com/ndrewcoding/escola/gamificacao/aplicacao/GeraSeloAlunoNovato.java
@@ -1,14 +1,25 @@
 package com.ndrewcoding.escola.gamificacao.aplicacao;
 
+import com.ndrewcoding.escola.gamificacao.dominio.selo.RepositorioDeSelos;
+import com.ndrewcoding.escola.gamificacao.dominio.selo.Selo;
+import com.ndrewcoding.escola.shared.dominio.CPF;
 import com.ndrewcoding.escola.shared.dominio.evento.Evento;
 import com.ndrewcoding.escola.shared.dominio.evento.Ouvinte;
 import com.ndrewcoding.escola.shared.dominio.evento.TipoDeEvento;
 
 public class GeraSeloAlunoNovato extends Ouvinte {
 
+    private final RepositorioDeSelos repositorioDeSelos;
+
+    public GeraSeloAlunoNovato(RepositorioDeSelos repositorioDeSelos) {
+        this.repositorioDeSelos = repositorioDeSelos;
+    }
+
     @Override
     protected void reageAo(Evento evento) {
-
+        CPF cpfDoAluno = (CPF) evento.informacoes().get("cpfDoAluno");
+        Selo seloDeNovato = new Selo(cpfDoAluno, "Novato");
+        repositorioDeSelos.cadastrar(seloDeNovato);
     }
 
     @Override
