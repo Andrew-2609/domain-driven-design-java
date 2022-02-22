fa5e0b1466b955f51fed3d2bf88584b5d46a4abb
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-21 21:10:45 -0300

    Feat: publish the event GeraSeloAlunoNovato in MatricularAlunoPorLinhaDeComando

--- src/main/java/com/ndrewcoding/escola/MatricularAlunoPorLinhaDeComando.java
+++ src/main/java/com/ndrewcoding/escola/MatricularAlunoPorLinhaDeComando.java
@@ -1,10 +1,12 @@
-package com.ndrewcoding.escola.academico;
+package com.ndrewcoding.escola;
 
-import com.ndrewcoding.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDTO;
 import com.ndrewcoding.escola.academico.aplicacao.aluno.matricular.MatricularAluno;
-import com.ndrewcoding.escola.shared.dominio.evento.PublicadorDeEventos;
+import com.ndrewcoding.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDTO;
 import com.ndrewcoding.escola.academico.dominio.aluno.LogDeAlunoMatriculado;
 import com.ndrewcoding.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
+import com.ndrewcoding.escola.gamificacao.aplicacao.GeraSeloAlunoNovato;
+import com.ndrewcoding.escola.gamificacao.infra.selo.RepositorioDeSelosEmMemoria;
+import com.ndrewcoding.escola.shared.dominio.evento.PublicadorDeEventos;
 
 public class MatricularAlunoPorLinhaDeComando {
     public static void main(String[] args) {
@@ -15,6 +17,7 @@ public class MatricularAlunoPorLinhaDeComando {
 
         PublicadorDeEventos publicadorDeEventos = new PublicadorDeEventos();
         publicadorDeEventos.adicionar(new LogDeAlunoMatriculado());
+        publicadorDeEventos.adicionar(new GeraSeloAlunoNovato(new RepositorioDeSelosEmMemoria()));
 
         MatricularAluno matricularAluno = new MatricularAluno(new RepositorioDeAlunosEmMemoria(), publicadorDeEventos);
         matricularAluno.executar(new MatricularAlunoDTO(cpf, nome, email));
